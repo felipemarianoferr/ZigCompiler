16c260a4d8c01e8742ea07579ae6725ef2917995
felipemarianoferr <felipemf2@al.insper.edu.br>
2025-03-06 12:41:44 -0300

    nao sei v2

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class Tokenizer:
 
     def selectNext(self):
 
-        while self.position < len(self.source) and self.source[self.position] == ' ':
+        while self.position < len(self.source) and self.source[self.position] in (' ', '\n', '\r'):
             self.position += 1
         if self.position >= len(self.source):
             self.next = Token('EOF','')
