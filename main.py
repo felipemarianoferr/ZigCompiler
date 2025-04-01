86f0aefe24000ed8b2fb6a852a570f01e71227e7
felipemarianoferr <felipemf2@al.insper.edu.br>
2025-04-01 12:17:07 -0300

    fix input v2.2

--- main.py
+++ main.py
@@ -3,28 +3,12 @@ import sys
 
 if __name__ == "__main__":
 
-    # source = sys.argv[1]
+    source = sys.argv[1]
 
-    # try:
-    #     with open(source, "r") as arquivo:
-    #         code = arquivo.read()
-    # except FileNotFoundError:
-    #     code = source
-    code = """
-    {
-        a = 3;
-        b = 0;
-
-        while (a > 0) {
-            b = b + 2;
-            a = a - 1;
-        }
-
-        if (b == 6 && !(a > 0)) {
-            print(b);
-        } else {
-            print(a);
-        }
-    }
-    """
+    try:
+        with open(source, "r") as arquivo:
+            code = arquivo.read()
+    except FileNotFoundError:
+        code = source
+        
     Parser.run(code)
